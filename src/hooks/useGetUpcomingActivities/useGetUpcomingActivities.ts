3d14bfe056864d9f0e23e6ccceb079f02229c9b9
Davidvera27 <davidrestrepovera@gmail.com>
2025-07-27 14:37:49 -0500

    Conexión de la card al módulo http://localhost:5173/actividades

--- src/hooks/useGetUpcomingActivities/useGetUpcomingActivities.ts
+++ src/hooks/useGetUpcomingActivities/useGetUpcomingActivities.ts
@@ -2,10 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import { client } from "../../api/client";
 import type { Activity } from "../../types";
 
+interface UpcomingActivitiesResponse {
+  data: Activity[];
+  message: string;
+  success: boolean;
+}
+
 const getUpcomingActivities = () =>
-  client.get<{ data: Activity[] }>("/api/activities-upcoming");
+  client.get<UpcomingActivitiesResponse>("/api/activities-upcoming");
+
 export const useGetUpcomingActivities = () =>
   useQuery({
     queryKey: ["get-upcoming-activities"],
     queryFn: getUpcomingActivities,
+    staleTime: 2 * 60 * 1000, // 2 minutos
+    refetchOnWindowFocus: false,
+    retry: 3,
+    retryDelay: 1000,
   });
